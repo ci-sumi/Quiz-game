70e0141c2c982f521d7e71662af8814d142fc1d6
Sumi Tharayil <tharayilsurendransumi@gmail.com>
2024-06-21 16:26:57 +0100

    Add logic to the  quiz question

--- quiz.py
+++ quiz.py
@@ -1,1 +1,11 @@
-print("Welcome to the quiz")
+print("Welcome to the quiz:")
+play=input("Do you want to play the quiz:yes/no?").lower()
+if play !="yes":
+    quit()
+else:
+    answer =input("Who was the first President of the Palestine?").title()
+    if(answer=="Mahmoud Abbas"):
+        print("Correct")
+    else:
+        print("its in correct")
+    
